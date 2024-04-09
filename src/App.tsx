4843fdd36fe94fa0c6c0f0f2b587e5b84291fc8f
shoaib khan <shoaib.khan.880066@gmail.com>
2024-04-10 02:14:29 +0530

    implemented lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+
+/**lazy loading the components so that way the components will on be loaded when we actually need them to load */
+
+const Home = lazy(() => import("./pages/home"));
+const Search = lazy(() => import("./pages/search"));
+const Cart = lazy(() => import("./pages/cart"));
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
