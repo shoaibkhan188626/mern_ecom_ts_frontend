b19f63604a2da2ea610998ddbf91280d854897ce
shoaibkhan188626 <shoaib.khan.880066@gmail.com>
2024-05-04 15:12:57 +0530

    added utility types

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,16 @@
+import {
+  Elements,
+  PaymentElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useNewOrderMutation } from "../redux/api/orderAPI";
+import { resetCart } from "../redux/reducer/cartReducer";
+import { RootState } from "@reduxjs/toolkit/query";
+import { NewOrderRequest } from "../types/api-types";
+
